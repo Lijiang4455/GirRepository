0ed1c02d09f4da0072de3582104d914635b20e15
Lijiang4455 <702398100@qq.com>
2015-12-20 23:47:58 +0800

    rep
    rep

--- src/com/atguigu/githup/test/GitTest.java
+++ src/com/atguigu/githup/test/GitTest.java
@@ -5,5 +5,6 @@ public class GitTest {
 	public static void main(String[] args) {
 		System.out.println("hello githup服务器");
 		System.out.println("hello githup服务器2");
+		System.out.println("githup rep local");
 	}
 }
